fc7c8693230a32c93cfc795768d53bdf2b081a60
Carson H <caho2973@colorado.edu>
2023-09-07 17:00:58 -0600

    Initial commit

---
+++ Assignment_1_Carson_HolubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1_Carson_HolubApp.swift
+//  Assignment 1 Carson Holub
+//
+//  Created by CUBS Customer on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_1_Carson_HolubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  Assignment 1 Carson Holub
+//
+//  Created by CUBS Customer (Carson Holub) on 9/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack { //vertical stack
+            myName().padding(10)
+            
+            ZStack { // stacks are on top of each other, there is no format
+                myCircle()
+                    .padding(5)// puts padding around only the struct attached to
+                    .opacity(0.55) // changed the opacity of the circle it is attached to
+                    .offset(CGSize(width: 15, height: 0)) //https://developer.apple.com/documentation/swiftui/view/offset(_:)
+                myCatImage().padding(25) // the order they are in matters to what order they are in on the page
+                myHeart().padding(125)
+                    .offset(CGSize(width: -100, height: -75))
+            }
+        }
+    }
+}
+
+//after some exploring on the internet, this is just the struct that provides us the display
+struct ContentView_Previews:  PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .background(gradient()) //added a background using the gradient struct/function
+    }
+}
+
+struct myName: View { //struct for displaying my name (text)
+    var body: some View {
+            Text("Meow!")
+                .foregroundStyle(.white) //foreground of the TEXT
+                .font(.system(size: 48, weight: .black, design: .serif)) //https://developer.apple.com/documentation/swiftui/text
+    }
+}
+
+struct myCircle: View { //struct for displaying circle
+    var body: some View {
+            Circle()
+                .foregroundStyle(.red)
+                
+    }
+}
+
+struct myHeart: View {
+    var body: some View {
+            Image("redHeart")
+                .resizable() // this makes it so that it fits on the view, squished under what already exists in view (text/circle)
+                .scaledToFit()// this makes it the correct size in respect to aspect ratio/view of parent. This one helps the edges look better too for some reason instead of ".aspectRatio(contentMode: .fit)" which was also found: https://developer.apple.com/documentation/swiftui/image
+    }
+}
+
+struct myCatImage: View {
+    var body: some View {
+        Image("kittyCat")
+            .resizable()
+            .scaledToFit()
+            .clipShape(Circle())
+    }
+}
+
+struct gradient: View { // one of the "modifiers" I made, which I found through Apple Developer: https://developer.apple.com/documentation/swiftui/lineargradient
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.white, .blue]), startPoint: .bottom, endPoint: .top)
+    }
+}
